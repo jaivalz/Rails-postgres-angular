e36ef5e23ae779473dd2f8e4dc30dcd4a020f492
Jaime <jaivalz@yahoo.com>
2016-04-03 15:15:12 +0000

    Replaced ruby code with angular code at customers index and added data through customers js controller

--- app/assets/javascripts/customers_app.js
+++ app/assets/javascripts/customers_app.js
@@ -1,10 +1,34 @@
-var app = angular.module("customers",[]); 
+var app = angular.module('customers',[]); 
 
-app.controller("CustomerSearchController", [
-  "$scope",
-  function($scope) {
-    $scope.search = function(searchTerm) {
-      $scope.searchedFor = searchTerm;
+app.controller("CustomerSearchController", [ 
+          '$scope',                          
+  function($scope) {                         
+    $scope.customers = [];
+    $scope.search = function(searchTerm) {   
+      $scope.customers = [
+        {
+          "first_name":"Esteban",
+          "last_name":"Zamora",
+          "email":"zamora0@macgyver.net",
+          "username":"estibo",
+          "created_at":"2016-01-01",
+        },
+        {
+          "first_name":"Asbante",
+          "last_name":"Arozam",
+          "email":"arozam@rennerfisher.org",
+          "username":"arozam",
+          "created_at":"2016-01-01",
+        },
+        {
+          "first_name":"Basante",
+          "last_name":"Maronza",
+          "email":"maronza@somewhere.org",
+          "username":"maronza",
+          "created_at":"2016-01-01",
+        }
+      ]
+      
     }
   }
 ]);
